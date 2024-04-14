2f6b87db12c1d244b808430cf0acfe5b2ec2add4
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-04-14 19:17:42 +0100

    Clean Activities

--- src/Activity.java
+++ src/Activity.java
@@ -6,25 +6,25 @@ public class Activity {
     public enum ActivityType {
         // TYPE(distance, altimetry, repetitions, weight, hard)
         // distance and altimetry
-        STREET_RUNNING(10000, 50, 0, 0, 0),
+        /* STREET_RUNNING(10000, 50, 0, 0, 0),
         TRAIL_RUNNING(15000, 500, 0, 0, 1),
         MOUNTAIN_BIKING(20000, 1000, 0, 0, 1),
-        STREET_BIKING(20000, 100, 0, 0, 0),
+        STREET_BIKING(20000, 100, 0, 0, 0), */
         // distance
         // (ex: remo, corrida na pista de atletismo, patinagem)
-        ROWING(10000, 0, 0, 0, 0),
+        /* ROWING(10000, 0, 0, 0, 0),
         TRACK_RUNNING,
-        SKATING,
+        SKATING, */
         // repetitions
         // (ex: abdominais, alongamentos, flexões)
-        ABDOMINALS,
+        /* ABDOMINALS,
         STRETCHING,
         PUSHUPS,
-        PULLUPS,
+        PULLUPS, */
         // weighted repetitions
         // (ex: levantamento de pesos, extensão de pernas)
-        WEIGHT_LIFTING,
-        LEG_EXTENSION,
+        /* WEIGHT_LIFTING,
+        LEG_EXTENSION, */
     }
 
     private int duration; // in minutes
