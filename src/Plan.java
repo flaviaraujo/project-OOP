e69ac3ba433f038a151133e6c3c48f4eb6f5dc53
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-04-15 19:25:13 +0100

    Add src/Plan.java brainstorm

--- src/Plan.java
+++ src/Plan.java
@@ -1,4 +1,27 @@
+import java.util.Date;
+
 /**
  * Plan
  */
-public class Plan {}
+public class Plan {
+    // private String name; // TODO is worth it? (to delete plans this must be unique)
+    private User user;
+    private Date date; // Is a plan for a specific date or a range of dates?
+    // private ArrayList<Events> events;
+    // public class Event {
+    //     private Activity activity;
+    //     private int activityRepetitions; // number of times the activity should be repeated
+    //     // Time or DateTime for start and end? Depends if plan is for a specific date or a range of dates
+    //     private DateTime start;
+    //     private DateTime end; // (start + activity.getDuration() * activityRepetitions)
+    //     private boolean completed; // TODO is worth it?
+    // }
+
+    // TODO
+    // Constructors
+    // Getters and Setters
+    // Methods - toString, clone, equals
+    // Methods - create (interactive) -> N (max 3) events for a plan and then Event.create() N times
+    // Methods - create (based on user goals) -> based on type of activities, recorrencia, etc
+    // Methods - remove
+}
