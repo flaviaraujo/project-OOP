8ba5796ea23bfbf6d2614ed92779f0d32d56580f
flaviaraujo <flaviaraujo28@gmail.com>
2024-04-15 20:38:30 +0100

    Added the intensity on Activity

--- src/Activity.java
+++ src/Activity.java
@@ -8,7 +8,7 @@ public abstract class Activity {
 
     public Activity() {
         this.duration = 0;
-        this.type = null;
+        this.intensity = 0;
     }
 
     public Activity(int duration, int intensity) {
@@ -18,14 +18,14 @@ public abstract class Activity {
 
     public Activity(Activity activity) {
         this.duration = activity.getDuration();
-        this.type = activity.getType();
+        this.intensity = activity.getIntensity();
     }
 
     public int getDuration() {
         return this.duration;
     }
 
-    public Type getIntensity() {
+    public int getIntensity() {
         return this.intensity;
     }
 
@@ -36,39 +36,6 @@ public abstract class Activity {
     public void setIntensity(int intensity) {
         this.intensity = intensity;
     }
+
+    public abstract int caloriesBurned(User u);
 }
-
-public class Distance extends Activity {}
-
-public class DistanceAltimetry extends Activity {
-    private int distance;
-    private int altimetry;
-
-    public DistanceAltimetry() {
-        super();
-        this.distance = 0;
-        this.altimetry = 0;
-    }
-
-    public DistanceAltimetry(int duration, int intensity, int distance, int altimetry) {
-        super(duration, intensity);
-        this.distance = distance;
-        this.altimetry = altimetry;
-    }
-
-    public DistanceAltimetry(DistanceAltimetry distanceAltimetry) {
-        super(distanceAltimetry);
-        this.distance = distanceAltimetry.getDistance();
-        this.altimetry = distanceAltimetry.getAltimetry();
-    }
-
-    @Override
-    public int caloriesBurned(User u) {
-        // TODO Calculate calories burned based on user, distance, altimetry, duration, and intensity
-        return 0;
-    }
-}
-
-public class Repetitions extends Activity {}
-
-public class RepetitionsWeight extends Activity {}
