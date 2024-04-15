fbeacc33efd882109b1ae71e11cae9ec1b88cd29
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-04-15 15:22:20 +0100

    Update src/Activity.java: enum type

--- src/Activity.java
+++ src/Activity.java
@@ -3,28 +3,83 @@
  */
 public class Activity {
 
-    public enum ActivityType {
-        // TYPE(distance, altimetry, repetitions, weight, hard)
+    public enum Type {
+        // TYPE         (dist, alt, reps, weight, hard)
+
         // distance and altimetry
-        /* STREET_RUNNING(10000, 50, 0, 0, 0),
-        TRAIL_RUNNING(15000, 500, 0, 0, 1),
-        MOUNTAIN_BIKING(20000, 1000, 0, 0, 1),
-        STREET_BIKING(20000, 100, 0, 0, 0), */
+        STREET_RUNNING  (1000,  50, 0, 0, false),
+        TRAIL_RUNNING   (1500, 300, 0, 0, true),
+        MOUNTAIN_BIKING (3000, 500, 0, 0, true),
+        STREET_BIKING   (5000, 250, 0, 0, false),
+
         // distance
-        // (ex: remo, corrida na pista de atletismo, patinagem)
-        /* ROWING(10000, 0, 0, 0, 0),
-        TRACK_RUNNING,
-        SKATING, */
+        ROWING        (5000, 0, 0, 0, false),
+        TRACK_RUNNING (2000, 0, 0, 0, false),
+        SKATING       (1500, 0, 0, 0, false),
+
         // repetitions
-        // (ex: abdominais, alongamentos, flexões)
-        /* ABDOMINALS,
-        STRETCHING,
-        PUSHUPS,
-        PULLUPS, */
+        ABDOMINALS      (0, 0, 50, 0, false),
+        STRETCHING      (0, 0, 25, 0, false),
+        PUSHUPS         (0, 0, 30, 0, false),
+        DIAMOND_PUSHUPS (0, 0, 20, 0, false),
+        PULLUPS         (0, 0, 10, 0, false),
+        DIPS            (0, 0, 15, 0, false),
+        MUSCLEUPS       (0, 0,  5, 0, true),
+
         // weighted repetitions
-        // (ex: levantamento de pesos, extensão de pernas)
-        /* WEIGHT_LIFTING,
-        LEG_EXTENSION, */
+        WEIGHT_LIFTING_SMALL  (0, 0, 10,  20, false),
+        WEIGHT_LIFTING_MEDIUM (0, 0, 10,  40, false),
+        WEIGHT_LIFTING_LARGE  (0, 0, 10,  60, true),
+        LEG_EXTENSION_SMALL   (0, 0, 10,  40, false),
+        LEG_EXTENSION_MEDIUM  (0, 0, 10,  60, false),
+        LEG_EXTENSION_LARGE   (0, 0, 10, 100, true);
+
+        private int distance, altimetry, repetitions, weight;
+        private boolean hard;
+
+        Type(int distance, int altimetry, int repetitions, int weight, boolean hard) {
+            this.distance = distance;
+            this.altimetry = altimetry;
+            this.repetitions = repetitions;
+            this.weight = weight;
+            this.hard = hard;
+        }
+
+        public int getDistance() {
+            return this.distance;
+        }
+
+        public int getAltimetry() {
+            return this.altimetry;
+        }
+
+        public int getRepetitions() {
+            return this.repetitions;
+        }
+
+        public int getWeight() {
+            return this.weight;
+        }
+
+        public boolean isDistanceAltimetry() {
+            return this.distance > 0 && this.altimetry > 0;
+        }
+
+        public boolean isDistance() {
+            return this.distance > 0 && this.altimetry == 0;
+        }
+
+        public boolean isRepetitions() {
+            return this.repetitions > 0 && this.weight == 0;
+        }
+
+        public boolean isWeightedRepetitions() {
+            return this.repetitions > 0 && this.weight > 0;
+        }
+
+        public boolean isHard() {
+            return this.hard;
+        }
     }
 
     private int duration; // in minutes
