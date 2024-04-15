bb3cac4a2b4d1e10ed81580349e0e8c2e1d8db92
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-04-15 15:23:49 +0100

    Add manage users option, load and save state

--- src/Main.java
+++ src/Main.java
@@ -11,67 +11,131 @@ import java.util.Scanner;
  */
 public class Main {
 
-    private ArrayList<Activity> createdActivities = new ArrayList<>();
-    private ArrayList<User> createdUsers = new ArrayList<>();
-    private ArrayList<Plan> createdPlans = new ArrayList<>();
+    private boolean updatedState = false;
+    private ArrayList<User> users = new ArrayList<>(); // currently loaded users
 
     public static void main(String[] args) {
-        Main main = new Main();
-        String filepath = "data/state.ser";
+        Main m = new Main();
+        String stateFilepath = "data/state.ser"; // default state file path
+        Scanner sc = new Scanner(System.in);
         try {
             // Parse command line argument
             if (args.length > 0) {
-                filepath = args[0];
-                loadState(filepath);
+                stateFilepath = args[0];
+                loadState(stateFilepath, m, sc);
             }
 
             System.out.println("Welcome to the Activity Planner!");
 
             // Main loop
             while (true) {
-                Scanner sc = new Scanner(System.in);
-                mainMenu(sc, filepath, main);
+                mainMenu(sc, stateFilepath, m);
             }
         } catch (Exception e) {
             System.out.println("Interrupted: " + e.getMessage());
         }
+        sc.close();
     }
 
-    private static void mainMenu(Scanner scanner, String filepath, Main main) {
+    private static void mainMenu(Scanner sc, String stateFilepath, Main m) {
+        System.out.println();
         System.out.println("[Main menu] Please select an option:");
-        System.out.println("(1) Create users, activities and plans");
-        System.out.println("(2) Create plans based on user objectives");
-        System.out.println("(3) Start a simulation");
-        System.out.println("(4) Statistics menu");
-        System.out.println("(5) Save program state");
-        System.out.println("(6) Load program state");
-        System.out.println("(7) Exit");
+        System.out.println("(1) Manage users (create, view, delete)");
+        System.out.println("(2) Create plan for an user");
+        System.out.println("(3) Create plans based on user objectives");
+        System.out.println("(4) Start a simulation");
+        System.out.println("(5) Statistics menu");
+        System.out.println("(6) Save program state");
+        System.out.println("(7) Load program state");
+        System.out.println("(8) Exit");
         System.out.print("Option: ");
-        int option = scanner.nextInt();
+        int option = 0;
+        try {
+            option = sc.nextInt();
+        }
+        catch (Exception e) {
+            sc.nextLine(); // clear buffer
+            // e.printStackTrace(); // removed to avoid clutter
+        }
         switch (option) {
             case 1:
-                // Create users, activities and plans
-                break;
+                while (true) {
+                    System.out.println();
+                    System.out.println("[Manage users menu] Please select an option:");
+                    System.out.println("(1) Create an user");
+                    System.out.println("(2) View an user");
+                    System.out.println("(3) Delete an user");
+                    System.out.println("(4) View all users");
+                    System.out.println("(5) Back to main menu");
+                    System.out.print("Option: ");
+                    int manageUsersOption = 0;
+                    try {
+                        manageUsersOption = sc.nextInt();
+                    }
+                    catch (Exception e) {
+                        sc.nextLine(); // clear buffer
+                    }
+                    switch (manageUsersOption) {
+                        case 1:
+                            // Create an user
+                            User user = User.create(sc, m.users);
+
+                            // Add user to the loaded users array
+                            m.users.add(user);
+                            // state is updated when a new user is created
+                            m.updatedState = true;
+                            break;
+                        case 2:
+                            // TODO is it worth to implement: view nultiple users by their name?
+                            User.view(sc, m.users);
+                            break;
+                        case 3:
+                            // Delete an user
+                            User.delete(sc, m.users);
+                            break;
+                        case 4:
+                            // View all users
+                            for (User u : m.users) System.out.println(u);
+                            break;
+                        case 5:
+                            // Back to main menu
+                            return;
+                        default:
+                            System.out.println("Invalid option");
+                            break;
+                    }
+                }
             case 2:
-                // Create plans based on user objectives
+                // TODO Create plan for an user
                 break;
             case 3:
-                // Start a simulation
+                // TODO Create plan based on user objectives
                 break;
             case 4:
-                // Statistics menu
+                // TODO Start a simulation
                 break;
             case 5:
-                // Save program state
-                saveState(filepath, main);
+                // TODO Statistics menu
                 break;
             case 6:
-                // Load program state
-                loadState(filepath);
+                // Save program state
+                saveState(stateFilepath, m);
                 break;
             case 7:
+                // Load program state
+                loadState(stateFilepath, m, sc);
+                break;
+            case 8:
                 // state: to save or not to save
+                if (m.updatedState) {
+                    System.out.print("Do you want to save the current state before exiting? [y/n]: ");
+                    String save = sc.next();
+                    if (save.equals("y")) {
+                        saveState(stateFilepath, m);
+                    }
+                }
                 System.out.println("Exiting...");
+                sc.close();
                 System.exit(0);
             default:
                 System.out.println("Invalid option");
@@ -79,22 +143,52 @@ public class Main {
         }
     }
 
-    private static void saveState(String filepath, Main main) {
-        // save state
-        if (
-            main.createdActivities.size() == 0 &&
-            main.createdUsers.size() == 0 &&
-            main.createdPlans.size() == 0
-        ) {
+    private static void saveState(String stateFilepath, Main m) {
+        if (!m.updatedState) {
             System.out.println("No data to save");
             return;
         }
-        System.out.println("Saving state to " + filepath);
-        // TODO
+        System.out.println("Saving state to " + stateFilepath);
+        try {
+            FileOutputStream fileOut = new FileOutputStream(stateFilepath);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(m.users);
+            out.close();
+            fileOut.close();
+            System.out.println("State saved successfully, " + m.users.size() + " users saved.");
+            m.updatedState = false;
+        } catch (Exception e) {
+            System.out.println("Error saving state: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
-    private static void loadState(String filepath) {
-        // load state
-        // TODO
+    private static void loadState(String stateFilepath, Main m, Scanner sc) {
+        if (m.updatedState) {
+            System.out.print("Warning: current state will be lost. Do you want to continue? [y/n]: ");
+            String cont = sc.next();
+            if (!cont.equals("y")) {
+                return;
+            }
+        }
+        System.out.println("Loading state from " + stateFilepath);
+        try {
+            FileInputStream fileIn = new FileInputStream(stateFilepath);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            ArrayList<?> tempUsers = (ArrayList<?>) in.readObject();
+            m.users = new ArrayList<>(tempUsers.size());
+            for (Object obj : tempUsers) {
+                if (obj instanceof User) {
+                    m.users.add((User) obj); // TODO should I clone the obj and them destroy him?
+                }
+            }
+            in.close();
+            fileIn.close();
+            System.out.println("State loaded successfully, " + m.users.size() + " users loaded.");
+            m.updatedState = false;
+        } catch (Exception e) {
+            System.out.println("Error loading state: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,21 +1,24 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.Serializable;
 
 /**
  * User
  * TODO: Dúvida em agregação é necessário clone de String's?
  */
-public class User {
+public class User implements Serializable {
 
-    public enum UserType {
-        // OLYMPIC(98),
-        PROFESSIONAL(90),
+    // private static final long serialVersionUID = 1L; // TODO: check if necessary
+
+    public enum Type implements Serializable {
+        OCCASIONAL(40),
         AMATEUR(60),
-        OCCASIONAL(40);
+        PROFESSIONAL(90);
+        // OLYMPIC(98);
 
         private int nutritionMultiplier;
 
-        UserType(int nutritionMultiplier) {
+        Type(int nutritionMultiplier) {
             this.nutritionMultiplier = nutritionMultiplier;
         }
 
@@ -24,51 +27,52 @@ public class User {
         }
     }
 
+    private int id;
     private String name;
     private String email;
     private String address;
     private int heartRate; // BPM
-    private UserType userType;
-    private ArrayList<Activity> activities;
-
+    private Type type;
+    // private Dictionary<Int, ArrayList<Plan>> plans; // TODO: multiple plans for an user?
+ 
     /* Default Constructor */
     public User() {
+        this.id = 0;
         this.name = "";
         this.email = "";
         this.address = "";
         this.heartRate = 0;
-        this.userType = UserType.OCCASIONAL;
-        this.activities = new ArrayList<Activity>();
+        this.type = Type.OCCASIONAL;
     }
 
     /* Parameterized Constructor */
     public User(
-        String name,
-        String email,
-        String address,
-        int heartRate,
-        UserType userType,
-        ArrayList<Activity> activities
+        int id, String name, String email,
+        String address, int heartRate, Type type
     ) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.address = address;
         this.heartRate = heartRate;
-        this.userType = userType;
-        this.activities = activities;
+        this.type = type;
     }
 
     /* Copy Constructor */
     public User(User user) {
+        this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
         this.address = user.getAddress();
         this.heartRate = user.getHeartRate();
-        this.userType = user.getUserType();
-        this.activities = user.getActivities();
+        this.type = user.getType();
     }
 
     /* Instance Methods - Getters */
+    public int getId() {
+        return this.id;
+    }
+
     public String getName() {
         return this.name;
     }
@@ -85,19 +89,15 @@ public class User {
         return this.heartRate;
     }
 
-    public UserType getUserType() {
-        return this.userType;
-    }
-
-    public ArrayList<Activity> getActivities() {
-        ArrayList<Activity> activities = new ArrayList<Activity>();
-        for (Activity activity : this.activities) {
-            // activities.add(new Activity(activity)); // TODO clone
-        }
-        return activities;
+    public Type getType() {
+        return this.type;
     }
 
     /* Instance Methods - Setters */
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -114,31 +114,26 @@ public class User {
         this.heartRate = heartRate;
     }
 
-    public void setUserType(UserType userType) {
-        this.userType = userType;
-    }
-
-    public void setActivities(ArrayList<Activity> activities) {
-        this.activities = new ArrayList<Activity>();
-        for (Activity activity : activities) {
-            // this.activities.add(new Activity(activity)); // TODO clone
-        }
+    public void setType(Type type) {
+        this.type = type;
     }
 
     /* Object methods */
     public String toString() {
         StringBuffer sb = new StringBuffer();
         sb.append("User {\n");
+        sb.append("  id: " + this.id + ",\n");
         sb.append("  name: " + this.name + ",\n");
         sb.append("  email: " + this.email + ",\n");
         sb.append("  address: " + this.address + ",\n");
         sb.append("  heartRate: " + this.heartRate + " BPM,\n");
-        sb.append("  userType: " + this.userType + ",\n");
-        sb.append("  activities: [\n");
-        for (Activity activity : this.activities) {
-            sb.append("    " + activity.toString() + ",\n");
-        }
-        sb.append("  ]\n}");
+        sb.append("  type: " + this.type + ",\n");
+        // sb.append("  activities: [\n");
+        // for (Activity activity : this.activities) {
+        //     sb.append("    " + activity.toString() + ",\n");
+        // }
+        // sb.append("  ]\n");
+        sb.append("}");
         return sb.toString();
     }
 
@@ -149,12 +144,12 @@ public class User {
 
         User user = (User) o;
         return (
+            user.getId() == this.id &&
             user.getName().equals(this.name) &&
             user.getEmail().equals(this.email) &&
             user.getAddress().equals(this.address) &&
             user.getHeartRate() == this.heartRate &&
-            user.getUserType() == this.userType &&
-            user.getActivities().equals(this.activities)
+            user.getType() == this.type
         );
     }
 
@@ -162,30 +157,191 @@ public class User {
         return new User(this);
     }
 
-    public User createUser(Scanner sc) { //TODO edge cases
-        System.out.println("Enter your name:");
-        String name = sc.nextLine();
-        System.out.println("Enter the your email:");
-        String email = sc.nextLine();
-        System.out.println("Enter the your address:");
-        String address = sc.nextLine();
-        System.out.println("Enter the your heart rate:");
-        int heartRate = sc.nextInt();
-        System.out.println("Enter the your type:");
-        System.out.println("  1 - PROFESSIONAL");
-        System.out.println("  2 - AMATEUR");
-        System.out.println("  3 - OCCASIONAL");
-        int userType = sc.nextInt();
+    public static User create(Scanner sc, ArrayList<User> users) {
         sc.nextLine();
-        System.out.println("Enter the your activities:");
-        ArrayList<Activity> activities = new ArrayList<Activity>();
-        return new User(
-            name,
-            email,
-            address,
-            heartRate,
-            userType == 1 ? UserType.PROFESSIONAL : userType == 2 ? UserType.AMATEUR : UserType.OCCASIONAL,
-            activities
-        );
+
+        String name, email, address;
+        int heartRate;
+        Type type;
+
+        // increment the last user id to get the new user id
+        int id = users.size() == 0 ? 1 : users.get(users.size() - 1).getId() + 1;
+
+        // TODO add upper limit
+        while (true) {
+            System.out.print("Enter user full name: ");
+            name = sc.nextLine();
+            // check if name is between 3 and 256 characters
+            if (name.length() < 3 || name.length() > 256) {
+                System.out.println("Name must be between 3 and 256 characters.");
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            System.out.print("Enter the user email: ");
+            String emailBuffer = sc.nextLine();
+            // check if email is valid using regex
+            if (!emailBuffer.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
+                System.out.println("Email is not valid.");
+                continue;
+            }
+            // check if email is 320 characters or less
+            if (emailBuffer.length() > 320) {
+                System.out.println("Email must be 320 characters or less.");
+                continue;
+            }
+            // check if email is unique
+            boolean isUnique = users.stream().noneMatch(user -> user.getEmail().equals(emailBuffer));
+            if (!isUnique) {
+                System.out.println("Email already exists.");
+                continue;
+            }
+            email = emailBuffer;
+            break;
+        }
+
+        while (true) {
+            System.out.print("Enter the user address: ");
+            address = sc.nextLine();
+            // check if address is between 5 and 1024 characters
+            if (address.length() < 5 || address.length() > 1024) {
+                System.out.println("Address must be between 5 and 1024 characters.");
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            System.out.print("Enter the user resting heart rate (in BPM): ");
+            try {
+                heartRate = sc.nextInt();
+            }
+            catch (Exception e) {
+                System.out.println("Invalid heart rate.");
+                sc.nextLine(); // clear buffer
+                continue;
+            }
+            // check if heart rate is between 20 and 200
+            if (20 > heartRate || heartRate > 200) {
+                System.out.println("Heart rate must be between 20 and 200 BPM.");
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            System.out.println("Possible user types:");
+            System.out.println("  1 - OCCASIONAL");
+            System.out.println("  2 - AMATEUR");
+            System.out.println("  3 - PROFESSIONAL");
+            // System.out.println("  4 - OLYMPIC");
+            System.out.print("Enter the user type: ");
+            int typeCode = 0;
+            try {
+                typeCode = sc.nextInt();
+            }
+            catch (Exception e) {
+                System.out.println("Invalid user type.");
+                sc.nextLine(); // clear buffer
+                continue;
+            }
+            // check if user type is valid
+            if (typeCode < 1 || typeCode > 3) {
+                System.out.println("Invalid user type.");
+                continue;
+            }
+            type =
+                typeCode == 1 ? Type.OCCASIONAL :
+                typeCode == 2 ? Type.AMATEUR :
+                Type.PROFESSIONAL;
+            break;
+        }
+
+        sc.nextLine(); // clear buffer
+
+        System.out.println("User created successfully. (ID: " + id + ")");
+
+        return new User(id, name, email, address, heartRate, type); // TODO clone
+    }
+
+    private static User search(Scanner sc, ArrayList<User> users) {
+
+        sc.nextLine(); // clear buffer
+
+        int option;
+        while (true) {
+            System.out.println();
+            System.out.println("Delete an user:");
+            System.out.println("(1) By ID");
+            System.out.println("(2) By email");
+            System.out.print("Option: ");
+            try {
+                option = sc.nextInt();
+            } catch (Exception e) {
+                sc.nextLine(); // clear buffer
+                System.out.println("Invalid option.");
+                continue;
+            }
+            if (option == 1 || option == 2) {
+                sc.nextLine(); // clear buffer
+                break;
+            }
+            System.out.println("Invalid option.");
+        }
+
+        int id;
+        String email;
+        User user = null;
+
+        if (option == 1) {
+            System.out.print("Enter the user ID: ");
+            try {
+                id = sc.nextInt();
+            } catch (Exception e) {
+                sc.nextLine(); // clear buffer
+                System.out.println("Invalid ID.");
+                return null;
+            }
+            user = users.stream()
+                .filter(u -> u.getId() == id)
+                .findFirst()
+                .orElse(null);
+        } else {
+            System.out.print("Enter the user email: ");
+            email = sc.nextLine();
+            user = users.stream()
+                .filter(u -> u.getEmail().equals(email))
+                .findFirst()
+                .orElse(null);
+        }
+
+        return user;
+    }
+
+    public static void view(Scanner sc, ArrayList<User> users) {
+
+        User user = search(sc, users);
+
+        if (user == null) {
+            System.out.println("User not found.");
+            return;
+        }
+
+        System.out.println(user);
+    }
+
+    public static void delete(Scanner sc, ArrayList<User> users) {
+
+        User user = search(sc, users);
+
+        if (user == null) {
+            System.out.println("User not found.");
+            return;
+        }
+
+        users.remove(user);
+        System.out.println("User deleted successfully.");
     }
 }
