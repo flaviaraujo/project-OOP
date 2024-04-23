0f519357a58ba73f1b52278aff9a40e6791f7cad
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-04-23 23:01:21 +0100

    Update create plan based on user goals

--- src/Plan.java
+++ src/Plan.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Scanner;
 
 import java.time.LocalTime;
@@ -176,7 +177,7 @@ public class Plan implements Serializable {
         }
 
         public Activity getActivity() {
-            return this.activity;
+            return this.activity.clone();
         }
 
         public int getRepetitionsLeft() {
@@ -189,16 +190,21 @@ public class Plan implements Serializable {
 
         public ArrayList<ActivityRepetition> clean(ArrayList<ActivityRepetition> ars) {
             // remove activities with 0 repetitions left
-            for (ActivityRepetition ar : ars) {
+            Iterator<ActivityRepetition> iterator = ars.iterator();
+            while (iterator.hasNext()) {
+                ActivityRepetition ar = iterator.next();
                 if (ar.getRepetitionsLeft() == 0) {
-                    ars.remove(ar);
+                    iterator.remove();
                 }
             }
             return ars;
         }
 
-        public ArrayList<ActivityRepetition> sort(ArrayList<ActivityRepetition> ar, int maxActivitiesPerDay, User user) {
-
+        public ArrayList<ActivityRepetition> sort(
+            ArrayList<ActivityRepetition> ar,
+            int maxActivitiesPerDay,
+            User user
+        ) {
             ar = clean(ar);
 
             Comparator<ActivityRepetition> activityRepetitionComparator = (ar1, ar2) -> {
@@ -211,43 +217,40 @@ public class Plan implements Serializable {
             return ar;
         }
 
-        public ArrayList<ActivityRepetition> createNonHard(ArrayList<Activity> a, int maxRepetitionPerWeek, int maxActivitiesPerDay, User user) {
-
+        public ArrayList<ActivityRepetition> create(
+            ArrayList<Activity> a,
+            int maxRepetitionPerWeek,
+            int maxActivitiesPerDay,
+            User user,
+            boolean isHard
+        ) {
             ArrayList<ActivityRepetition> ar = new ArrayList<ActivityRepetition>();
+
             for (Activity activity : a) {
-                if (!activity.getHard()) {
+                if (activity.getHard() == isHard) {
                     ar.add(new ActivityRepetition(activity, maxRepetitionPerWeek));
                 }
             }
-            ar = sort(ar, maxActivitiesPerDay, user);
-            return ar;
-        }
 
-        public ArrayList<ActivityRepetition> createHard(ArrayList<Activity> a, int maxRepetitionPerWeek, int maxActivitiesPerDay, User user) {
-            ArrayList<ActivityRepetition> ar = new ArrayList<ActivityRepetition>();
-            for (Activity activity : a) {
-                if (activity.getHard()) {
-                    ar.add(new ActivityRepetition(activity, maxRepetitionPerWeek));
-                }
-            }
-            ar = sort(ar, 1, user);
+            ar = sort(ar, isHard ? 1 : maxActivitiesPerDay, user);
             return ar;
         }
     }
 
     // create plan based on user goals
-    public Plan createBasedOnGoals(User user, Scanner sc) {
-        // restrictions
-        // hard activituies can't be in the same day and consecutive days
-        // max activities per day (max 3)
-        // min distinct activities per day (max 3)
-        // nRepActivityPerWeek (min 1, no max)
+    public Plan createBasedOnGoals(Scanner sc, User user) {
 
-        int caloriesGoal = 12000;
-        int maxActivitiesPerDay = 3;
-        int maxDisitinctActivitiesPerDay = 3;
-        int nActivityRepetitionPerWeek = 4;
-        ArrayList<Activity> selectedActivities = user.getActivities();
+        if (user.getActivities().isEmpty()) {
+            System.out.println("There are no activities available.");
+            System.out.println("Please add activities before trying to generate a plan.");
+            return null;
+        }
+
+        int caloriesGoal;
+        int maxActivitiesPerDay;
+        int maxDisitinctActivitiesPerDay;
+        int nActivityRepetitionPerWeek;
+        ArrayList<Activity> selectedActivities = new ArrayList<Activity>();
 
         IO io = new IO();
 
@@ -294,22 +297,16 @@ public class Plan implements Serializable {
         while (true) {
             System.out.print("Enter number of your activities to select: ");
             int nActivities = io.readInt(sc);
-            ArrayList<Activity> activities = user.getActivities();
-            if (nActivities < 1 || nActivities > user.getActivities().size()) {
-                System.out.println("Invalid number of hard activities. Please enter a number between 1 and " +
+            ArrayList<Activity> userActivities = user.getActivities();
+            if (nActivities < 1 || nActivities > userActivities.size()) {
+                System.out.println("Invalid number of activities. Please enter a number between 1 and " +
                     user.getActivities().size() + ".");
                 continue;
             }
 
-            for (int i = 0; i < nActivities; i++) {
-                // Remove selected activities from the list
-                ArrayList<Activity> toSearch = new ArrayList<Activity>();
-                for (Activity a : activities) {
-                    if (!selectedActivities.contains(a)) {
-                        toSearch.add(a);
-                    }
-                }
+            ArrayList<Activity> toSearch = new ArrayList<Activity>(userActivities);
 
+            for (int i = 0; i < nActivities; i++) {
                 System.out.println("Select activity number " + (i + 1) + ".");
                 Activity activity = Activity.search(sc, toSearch);
                 if (activity == null) {
@@ -319,37 +316,56 @@ public class Plan implements Serializable {
                 }
 
                 selectedActivities.add(activity);
+                toSearch.remove(activity);
             }
             break;
         }
 
-        int daysWithHardActivities = 0;
-        // boolean canBeHard = daysWithHardActivities == 0;
-        // reset: daysWithoutHardActivities = 2;
-        int distinctActivitiesPerDay = 0;
+        // restrictions
+        // hard activituies can't be in the same day and consecutive days
+        // max activities per day (max 3)
+        // min distinct activities per day (max 3)
+        // nRepActivityPerWeek (min 1, no max)
+
+        // days until the next hard activity is possible
+        int daysUntilHard = 0;
+        // boolean canBeHard: (daysUntilHard == 0);
+        // reset: daysUntilHard = 2;
+
+        // number of disitinct activities added in one day
+        int distinctCount = 0;
+
+        // total number of activities (for display purposes only)
+        int totalActivities = 0;
 
         // seperate activities by hard and not hard (sorted by calories desc)
         ActivityRepetition arAux = new ActivityRepetition();
+
         ArrayList<ActivityRepetition> activitiesHard =
-            arAux.createHard(selectedActivities, nActivityRepetitionPerWeek, maxActivitiesPerDay, user);
+            arAux.create(selectedActivities, nActivityRepetitionPerWeek, maxActivitiesPerDay, user, true);
+
         ArrayList<ActivityRepetition> activities =
-            arAux.createNonHard(selectedActivities, nActivityRepetitionPerWeek, maxActivitiesPerDay, user);
+            arAux.create(selectedActivities, nActivityRepetitionPerWeek, maxActivitiesPerDay, user, false);
+
+        ActivityRepetition ar = null;
+        Activity a = null;
+        int repetitionsLeft = 0;
+
+        ActivityRepetition arHard = null;
+        Activity aHard = null;
+        int repetitionsLeftHard = 0;
 
         ArrayList<Event> events = new ArrayList<Event>();
 
-        ActivityRepetition ar = null;
-        ActivityRepetition arHard = null;
-        Activity a = null;
-        Activity aHard = null;
-        int repetitionsLeft = 0;
-        int repetitionsLeftHard = 0;
+        // TODO if sunday is hard activity dat then saturday can't be hard activity day
+        // TODO add event times
 
         for (int day = 1; day <= 7; day++) {
 
-            distinctActivitiesPerDay = 0;
+            distinctCount = 0;
 
-            if (daysWithHardActivities > 0) {
-                daysWithHardActivities--;
+            if (daysUntilHard > 0) {
+                daysUntilHard--;
             }
 
             if (activitiesHard.isEmpty() && activities.isEmpty()) {
@@ -358,8 +374,12 @@ public class Plan implements Serializable {
 
             for (int i = 0; i < maxActivitiesPerDay;) {
 
+                if (activitiesHard.isEmpty() && activities.isEmpty()) {
+                    break;
+                }
+
                 if (!activities.isEmpty()) {
-                    ar = activitiesHard.get(0);
+                    ar = activities.get(0);
                     a = ar.getActivity();
                     repetitionsLeft = ar.getRepetitionsLeft();
                 }
@@ -378,35 +398,42 @@ public class Plan implements Serializable {
 
                 int nActivityOfThisType = Math.min(repetitionsLeft, maxActivitiesPerDay - i);
 
-                if (ar == null
-                    || (aHard.calculateCalories(user) >= a.calculateCalories(user) * nActivityOfThisType
-                    && daysWithHardActivities == 0))
+                if (ar == null ||
+                    (arHard != null &&
+                    aHard.calculateCalories(user) >= a.calculateCalories(user) * nActivityOfThisType &&
+                    daysUntilHard == 0))
                 {
                     // add hard activity (only one can be added)
-                    Event event = new Event(aHard, 1, day, LocalTime.of(0, 0));
+                    Event event = new Event(aHard, 1, day, LocalTime.of(8 + 4 * i, 0));
                     events.add(event);
 
                     // update repetitions
                     repetitionsLeftHard--;
                     arHard.setRepetitionsLeft(repetitionsLeftHard);
 
-                    // re-sort
-                    activitiesHard = arAux.sort(activitiesHard, 1, user);
+                    // hard activities logic (reset daysUntilHard)
+                    daysUntilHard = 2;
 
-                    // hard activities logic
-                    daysWithHardActivities = 2;
+                    // total activities counter
+                    totalActivities++;
 
                     // increment activity iterator
                     i++;
+
+                    // re-sort
+                    activitiesHard = arAux.sort(activitiesHard, 1, user);
                 }
                 else if (ar != null) {
                     // add normal activity
-                    Event event = new Event(a, nActivityOfThisType, day, LocalTime.of(0, 0));
+                    Event event = new Event(a, nActivityOfThisType, day, LocalTime.of(8 + 4 * i, 0));
                     events.add(event);
 
                     // update repetitions
                     repetitionsLeft -= nActivityOfThisType;
                     ar.setRepetitionsLeft(repetitionsLeft);
+
+                    // total activities counter
+                    totalActivities += nActivityOfThisType;
 
                     // increment activity iterator
                     i += nActivityOfThisType;
@@ -416,12 +443,14 @@ public class Plan implements Serializable {
                 }
 
                 // increment distinct activities counter
-                distinctActivitiesPerDay++;
-                if (distinctActivitiesPerDay >= maxDisitinctActivitiesPerDay) {
+                distinctCount++;
+                if (distinctCount >= maxDisitinctActivitiesPerDay) {
                     break;
                 }
             }
         }
+
+        int possibleActivitiesPerWeek = Math.min(maxActivitiesPerDay * 7, nActivityRepetitionPerWeek * selectedActivities.size());
 
         // Check if plan meets the calories goal
         int calories = 0;
@@ -430,11 +459,26 @@ public class Plan implements Serializable {
         }
         if (calories < caloriesGoal) {
             System.out.println("Based on your goals, it's impossible to create a plan.");
+            System.out.println("Total activities: " + totalActivities + " / " + possibleActivitiesPerWeek);
             System.out.println("Calories: " + calories + " / " + caloriesGoal);
             return null;
         }
 
-        System.out.println("Plan created successfully.");
-        return new Plan("Plan based on goals", events);
+        // create plan
+        Plan plan = new Plan("Plan based on goals", events);
+
+        // display plan and relevant info
+        System.out.println(plan.getName());
+        for (Event e : plan.getEvents()) {
+            System.out.println(e);
+            System.out.println("Expected calories: " +
+                e.getActivity().calculateCalories(user) * e.getActivityRepetitions());
+            System.out.println();
+        }
+        System.out.println("Total activities: " + totalActivities + " / " + possibleActivitiesPerWeek);
+        System.out.println("Calories: " + calories + " / " + caloriesGoal);
+        System.out.println("Plan generated successfully.");
+
+        return plan.clone();
     }
 }
